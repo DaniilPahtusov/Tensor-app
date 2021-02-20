a7d2274a9fdb874a6ce43fab7380d5b1535da341
TED755 <geka.tihomirov@yandex.ru>
2021-02-20 15:13:46 +0300

    Cheta napisal dalshe len potom

---
+++ app/methods.py
@@ -0,0 +1,6 @@
+class Methods:
+    def __init__(self):
+        pass
+
+    def registration():
+        pass
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import request
+import json
+# from methods import Methods
 
 @app.route('/')
 @app.route('/index')
@@ -10,9 +12,19 @@ def index():
 @app.route('/auth', methods=['GET', 'POST'])
 def auth():
     if request.method == "POST":
-        return 'post'
+        f = request.get_json()
+        print(f)
+        return '200'
     else:
         return 'get'
 
 # registration
-def register(): pass
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     if request.method == 'POST':
+#         pass
+
+# {"login": 'admin', "password": 'admin'}
+
+# curl --request POST --header 'Content-Type: application/json' --data '{"login": "Loh", "password": "Pidr"}' 'http://127.0.0.1:5000/auth'
+
